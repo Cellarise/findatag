73f3634c6fbfa077dae4a5e7ab7850cea4aa80e5
John Barry <cellarise@outlook.com>
2014-07-13 17:37:43 +1000

    MAR-49 #time 0.25 add a grunt task to generate locale properties from dust templates using an 'i' tag, so that the locale properties file can be automatically generated and used to produce locale property files for other locales.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+grunt-cellarise-findatag
+==========
+This package has been forked from paypal/findatag, and adds a grunt task to generate locale properties from dust templates using an 'i' tag, so that the locale properties file can be automatically generated and used to produce locale property files for other locales.
+
 findatag
 ==========
 
---
+++ tasks/findatagLocale.js
@@ -0,0 +1,54 @@
+module.exports = function (grunt) {
+    grunt.registerMultiTask("findatagLocale", "Task to generate locale properties.", function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var finder = require("findatag"),
+            done = this.async(),
+            self = this;
+        // Iterate over all src-dest file pairs.
+        this.files.forEach(function (f, idx) {
+            f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                var result = [];
+                // Read file source.
+                //var src = grunt.file.read(filepath);
+                // Process file
+                finder.parse(filepath, {
+                    tags: ['i'],
+                    onTag: function (def, callback) {
+                        var attr = def.attributes;
+                        if(attr.v === undefined) {
+                            result.push("");
+                            callback(null, "");
+                        } else {
+                            result.push(attr.n + "=" + attr.v);
+                            callback(null, attr.n + "=" + attr.v);
+                        }
+                    }
+                }, function () {
+                    // result is the full processed file
+                    // Write the destination file.
+                    grunt.file.write(f.dest, result.join("\n"));
+
+                    // Print a success message.
+                    grunt.log.writeln(f.dest + ' file created.');
+                    if(idx+1 === self.files.length){
+                        done();
+                    }
+                });
+
+
+                //return src;
+            });
+
+        });
+    });
+
+};
+
